68b86aea38348d4d6602d9d5b504697ae3bc25e5
Adit7643 <adityakumarjmp15@gmail.com>
2025-04-28 02:03:31 +0530

    added filament

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\RolesEnum;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -32,8 +33,15 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
+        $user = Auth::user();
+        $route = "/";
+        if($user->hasAnyRole([RolesEnum::Admin, RolesEnum::Vendor])) {
+            return Inertia::location('/admin');
+        }else if($user->hasRole(RolesEnum::User)) {
+            $route = route('/dashboard', absolute: false);
+        }
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended($route);
     }
 
     /**
